f8ef1af887b6c81ff71349a15b342a8a0160c11a
G M T Borges <gustavo@gmtborges.com>
2024-03-06 08:02:42 -0300

    improve app scripts and files

--- playbooks/app-scripts/files/run-app.sh
+++ playbooks/app-scripts/files/run-app.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Build and deploy
-docker image prune
-docker container prune
-docker container build -t app ./app
+docker container stop app
+docker container rm app
+docker container prune --force
+
+docker image prune --force
+export $(cat .env)
+docker image build -t app ./app
 docker container run -d -p 3000:3000 --env-file .env --name app app
--- playbooks/app-scripts/files/stop-app.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-docker container stop app
-docker container rm app
